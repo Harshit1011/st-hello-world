23589923a24d5e042b053dd5265c60819a951464
Harshit <gargharshit2000@gmail.com>
2024-08-22 12:59:17 +0530

    multi page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+from multipage import MultiPage
+from pages import page1, page2, page3  # Import your app modules here
 
-st.write('Hello world! new commit from local')
+# Create an instance of the app
+app = MultiPage()
+
+# Title of the main page
+st.title("Dummy Multipage Streamlit App")
+
+# Add all your applications (pages) here
+app.add_page("Page 1", page1.app)
+app.add_page("Page 2", page2.app)
+app.add_page("Page 3", page3.app)
+
+# The main app
+app.run()
