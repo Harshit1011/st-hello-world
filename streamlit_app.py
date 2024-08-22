7f2258b4f8bad16a95ae1dc177f488f4a6f4a398
Harshit <gargharshit2000@gmail.com>
2024-08-22 13:48:06 +0530

    multi page link 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,54 @@
+# import streamlit as st
+# from multipage import MultiPage
+# from pages import page1, page2, page3  # Import your app modules here
+
+# # Create an instance of the app
+# app = MultiPage()
+
+# # Title of the main page
+# st.title("Dummy Multipage Streamlit App")
+
+# # Add all your applications (pages) here
+# app.add_page("XPage 1", page1.app)
+# app.add_page("XPage 2", page2.app)
+# app.add_page("XPage 3", page3.app)
+
+# # The main app
+# app.run()
+
+
 import streamlit as st
-from multipage import MultiPage
-from pages import page1, page2, page3  # Import your app modules here
 
-# Create an instance of the app
-app = MultiPage()
+# Function to manage page navigation
+def navigate_to(page):
+    st.experimental_set_query_params(page=page)
 
-# Title of the main page
-st.title("Dummy Multipage Streamlit App")
+# Function to render page content
+def render_page(page):
+    if page == "page1":
+        st.title("Page 1")
+        st.write("Welcome to Page 1!")
+        st.markdown("[Go to Home](?page=home)")
+        st.markdown("[Go to Page 2](?page=page2)")
 
-# Add all your applications (pages) here
-app.add_page("XPage 1", page1.app)
-app.add_page("XPage 2", page2.app)
-app.add_page("XPage 3", page3.app)
+    elif page == "page2":
+        st.title("Page 2")
+        st.write("Welcome to Page 2!")
+        st.markdown("[Go to Home](?page=home)")
+        st.markdown("[Go to Page 1](?page=page1)")
 
-# The main app
-app.run()
+    else:
+        st.title("Home")
+        st.write("Welcome to the Home Page!")
+        st.markdown("[Go to Page 1](?page=page1)")
+        st.markdown("[Go to Page 2](?page=page2)")
+
+# Main function
+def main():
+    query_params = st.experimental_get_query_params()
+    page = query_params.get("page", ["home"])[0]
+
+    render_page(page)
+
+if __name__ == "__main__":
+    main()
