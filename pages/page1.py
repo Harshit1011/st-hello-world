23bac09bb49d23eda6665194b0c52c8be7bbcd79
Harshit <gargharshit2000@gmail.com>
2024-08-22 13:14:50 +0530

    multi page 3

--- pages/page1.py
+++ pages/page1.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def app():
-    st.title("Page 12")
+    st.title("Page 1")
     st.write("Welcome to Page 1")
--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,9 @@ app = MultiPage()
 st.title("Dummy Multipage Streamlit App")
 
 # Add all your applications (pages) here
-app.add_page("Page 1", page1.app)
-app.add_page("Page 2", page2.app)
-app.add_page("Page 3", page3.app)
+app.add_page("XPage 1", page1.app)
+app.add_page("XPage 2", page2.app)
+app.add_page("XPage 3", page3.app)
 
 # The main app
 app.run()
